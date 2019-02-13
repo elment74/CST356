3f4716ca0b2a59156e3be2d39aec12a60707f896
elment74 <elment74@gmail.com>
2019-02-13 15:13:41 -0800

    Week5 Submission

---
+++ Week5/webapp/src/components/Persons.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h2>Persons</h2>
+        <tr>
+            <th>First Name</th>
+            <th>Middle Initial</th>
+            <th>Last Name</th>
+        </tr>
+        <tr>
+            <th>Tung</th>
+            <th>H</th>
+            <th>Duong</th>
+        </tr>
+        <tr>
+            <th>John</th>
+            <th></th>
+            <th>Wick</th>
+        </tr>
+        <tr>
+            <th>Keanu</th>
+            <th>C</th>
+            <th>Reeves</th>
+        </tr>
+        <tr>
+            <th>Harrison</th>
+            <th></th>
+            <th>Ford</th>
+        </tr>
+        <tr>
+            <th>Takaya</th>
+            <th></th>
+            <th>Kuroda</th>
+        </tr>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+table, th, td{
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td{
+    padding: 5px;
+    text-align: left;
+}
+
+.TableHeader{
+    background-color: black;
+    color:white;
+}
+</style>
---
+++ Week5/webapp/src/components/Students.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h2 class = 'sectionHeading'>Students</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="student in Students" :key="student.id">
+                    <td>{{student.id}}</td>
+                    <td>{{student.email}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Students',
+    mounted() {
+        this.students = getStudent();
+    },
+    data() {
+        return {
+            students: []
+        }
+    }
+}
+
+function getStudents() {
+    return JSON.parse(testStudents).students;
+}
+
+var testStudents = '{"students": [{"id": "123456", "email":"Something@something.com"}]}'
+</script>
+
+
+<style scoped>
+
+</style>
